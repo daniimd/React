dbef4714bb6025d574a0f86be2c9903e61179a7b
Daniela Martins <dgarcia@webeleven.com.br>
2019-07-19 15:40:02 -0300

    Updates

---
+++ precache-manifest.db647f97a2a6e097aa6ae4536bc08b11.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4971844868c6747a5f91a94773c1bab8",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "c1862b55ea18bd2a62c2",
+    "url": "/React/static/css/main.ea723344.chunk.css"
+  },
+  {
+    "revision": "76cf9d608e498589c74b",
+    "url": "/React/static/js/2.254d5a14.chunk.js"
+  },
+  {
+    "revision": "c1862b55ea18bd2a62c2",
+    "url": "/React/static/js/main.eb4fc900.chunk.js"
+  },
+  {
+    "revision": "283b01c4b2c734b504b3",
+    "url": "/React/static/js/runtime~main.d53f88d2.js"
+  },
+  {
+    "revision": "485c1fea25ce256b74214b18951945d7",
+    "url": "/React/static/media/arrow.485c1fea.png"
+  },
+  {
+    "revision": "76b4418b551b4bbb105ce15937041723",
+    "url": "/React/static/media/background1.76b4418b.jpg"
+  },
+  {
+    "revision": "084e30d1453e6f12fb6f7cdada86a780",
+    "url": "/React/static/media/cadastro.084e30d1.jpg"
+  },
+  {
+    "revision": "cc730595b6860a1aa6d1662bce5b938f",
+    "url": "/React/static/media/dog.cc730595.jpg"
+  },
+  {
+    "revision": "dc0d0191edc3dfb7b2d5d5355e5cb52e",
+    "url": "/React/static/media/slide-1.dc0d0191.jpg"
+  },
+  {
+    "revision": "23cad2d69d459e043421d8d0d8b93bae",
+    "url": "/React/static/media/slide-2.23cad2d6.jpg"
+  }
+]);
